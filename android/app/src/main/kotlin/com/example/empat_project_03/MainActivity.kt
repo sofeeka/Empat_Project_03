c8d74300cfbd2816c37b88085d0b59dc6a6a2713
sofeeka <sofiiahrychukh@gmail.com>
2024-03-16 23:23:28 +0100

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/empat_project_03/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.empat_project_03
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Goodreads',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool showBorders = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
