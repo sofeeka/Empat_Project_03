7b2e667b7758546a3db653668c72852b0880d188
sofeeka <sofiiahrychukh@gmail.com>
2024-03-17 17:28:46 +0100

    Extracted MyNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -35,9 +35,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-  static const _iconColor = Colors.black;
-
   static const List<Widget> _widgetOptions = <Widget>[
     Text('Index 0: MyHomePage'),
     Text(
@@ -54,7 +51,9 @@ class _MyHomePageState extends State<MyHomePage> {
     ),
   ];
 
-  void _onItemTapped(int index) {
+  int _selectedIndex = 0;
+
+  void _onDestinationSelected(int index) {
     setState(() {
       _selectedIndex = index;
     });
@@ -65,47 +64,66 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: const MyAppBar(),
       body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: NavigationBar(
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.home_outlined, color: _iconColor),
-            label: 'Home',
-            selectedIcon: Icon(Icons.home, color: _iconColor),
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.bookmarks_outlined, color: _iconColor),
-            label: 'My Books',
-            selectedIcon: Icon(Icons.bookmarks, color: _iconColor),
-          ),
-          NavigationDestination(
-            icon: Icon(IconData(0xf037, fontFamily: 'MaterialIcons'),
-                color: _iconColor),
-            label: 'Home',
-            selectedIcon: Icon(
-              IconData(
-                0xe248,
-                fontFamily: 'MaterialIcons',
-              ),
-              color: _iconColor,
-            ),
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.search, color: _iconColor),
-            label: 'Search',
-            selectedIcon: Icon(Icons.search_outlined, color: _iconColor),
-          ),
-          NavigationDestination(
-            icon: Icon(
-              Icons.format_list_bulleted,
-              color: _iconColor,
-            ),
-            label: 'More',
-          ),
-        ],
+      bottomNavigationBar: MyNavigationBar(
         selectedIndex: _selectedIndex,
-        onDestinationSelected: _onItemTapped,
-        indicatorColor: Colors.transparent,
+        onDestinationSelected: _onDestinationSelected,
       ),
+    );
+  }
+}
+
+class MyNavigationBar extends StatelessWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onDestinationSelected;
+
+  const MyNavigationBar({super.key,
+    required this.selectedIndex,
+    required this.onDestinationSelected});
+
+  final Color _iconColor = Colors.black;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      destinations: [
+        NavigationDestination(
+          icon: Icon(Icons.home_outlined, color: _iconColor),
+          label: 'Home',
+          selectedIcon: Icon(Icons.home, color: _iconColor),
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.bookmarks_outlined, color: _iconColor),
+          label: 'My Books',
+          selectedIcon: Icon(Icons.bookmarks, color: _iconColor),
+        ),
+        NavigationDestination(
+          icon: Icon(const IconData(0xf037, fontFamily: 'MaterialIcons'),
+              color: _iconColor),
+          label: 'Home',
+          selectedIcon: Icon(
+            const IconData(
+              0xe248,
+              fontFamily: 'MaterialIcons',
+            ),
+            color: _iconColor,
+          ),
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.search, color: _iconColor),
+          label: 'Search',
+          selectedIcon: Icon(Icons.search_outlined, color: _iconColor),
+        ),
+        NavigationDestination(
+          icon: Icon(
+            Icons.format_list_bulleted,
+            color: _iconColor,
+          ),
+          label: 'More',
+        ),
+      ],
+      selectedIndex: selectedIndex,
+      onDestinationSelected: onDestinationSelected,
+      indicatorColor: Colors.transparent,
     );
   }
 }
