05a88598c51429f672a57d82b166783a459c45ef
sofeeka <sofiiahrychukh@gmail.com>
2024-03-17 17:05:07 +0100

    Added Footer (NavigationBar)

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromRGBO(192, 188, 169, 100),
+          primary: const Color.fromRGBO(192, 188, 169, 100),
         ),
         useMaterial3: true,
       ),
@@ -34,10 +35,77 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const _iconColor = Colors.black;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Index 0: MyHomePage'),
+    Text(
+      'Index 1: My Books',
+    ),
+    Text(
+      'Index 2: Home',
+    ),
+    Text(
+      'Index 3: Search',
+    ),
+    Text(
+      'Index 4: More',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(),
+      appBar: const MyAppBar(),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined, color: _iconColor),
+            label: 'Home',
+            selectedIcon: Icon(Icons.home, color: _iconColor),
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.bookmarks_outlined, color: _iconColor),
+            label: 'My Books',
+            selectedIcon: Icon(Icons.bookmarks, color: _iconColor),
+          ),
+          NavigationDestination(
+            icon: Icon(IconData(0xf037, fontFamily: 'MaterialIcons'),
+                color: _iconColor),
+            label: 'Home',
+            selectedIcon: Icon(
+              IconData(
+                0xe248,
+                fontFamily: 'MaterialIcons',
+              ),
+              color: _iconColor,
+            ),
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.search, color: _iconColor),
+            label: 'Search',
+            selectedIcon: Icon(Icons.search_outlined, color: _iconColor),
+          ),
+          NavigationDestination(
+            icon: Icon(
+              Icons.format_list_bulleted,
+              color: _iconColor,
+            ),
+            label: 'More',
+          ),
+        ],
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onItemTapped,
+        indicatorColor: Colors.transparent,
+      ),
     );
   }
 }
