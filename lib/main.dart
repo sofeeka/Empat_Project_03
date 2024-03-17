59270d7c21d7cd190c46382f026fd4a2b5e68540
sofeeka <sofiiahrychukh@gmail.com>
2024-03-17 17:30:31 +0100

    Moved MyNavigationBar to a new file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'MyNavigationBar.dart';
 import 'package:flutter/material.dart';
 
 import 'MyAppBar.dart';
@@ -68,62 +69,6 @@ class _MyHomePageState extends State<MyHomePage> {
         selectedIndex: _selectedIndex,
         onDestinationSelected: _onDestinationSelected,
       ),
-    );
-  }
-}
-
-class MyNavigationBar extends StatelessWidget {
-  final int selectedIndex;
-  final ValueChanged<int> onDestinationSelected;
-
-  const MyNavigationBar({super.key,
-    required this.selectedIndex,
-    required this.onDestinationSelected});
-
-  final Color _iconColor = Colors.black;
-
-  @override
-  Widget build(BuildContext context) {
-    return NavigationBar(
-      destinations: [
-        NavigationDestination(
-          icon: Icon(Icons.home_outlined, color: _iconColor),
-          label: 'Home',
-          selectedIcon: Icon(Icons.home, color: _iconColor),
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.bookmarks_outlined, color: _iconColor),
-          label: 'My Books',
-          selectedIcon: Icon(Icons.bookmarks, color: _iconColor),
-        ),
-        NavigationDestination(
-          icon: Icon(const IconData(0xf037, fontFamily: 'MaterialIcons'),
-              color: _iconColor),
-          label: 'Home',
-          selectedIcon: Icon(
-            const IconData(
-              0xe248,
-              fontFamily: 'MaterialIcons',
-            ),
-            color: _iconColor,
-          ),
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.search, color: _iconColor),
-          label: 'Search',
-          selectedIcon: Icon(Icons.search_outlined, color: _iconColor),
-        ),
-        NavigationDestination(
-          icon: Icon(
-            Icons.format_list_bulleted,
-            color: _iconColor,
-          ),
-          label: 'More',
-        ),
-      ],
-      selectedIndex: selectedIndex,
-      onDestinationSelected: onDestinationSelected,
-      indicatorColor: Colors.transparent,
     );
   }
 }
